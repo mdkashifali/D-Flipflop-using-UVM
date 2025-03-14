f1699466153b20715a340a81ab7f43d36c6a4c95
Kashif <mdkashifali18081999@gmail.com>
2025-03-15 00:18:11 +0530

    Create dff.sv

---
+++ dff.sv
@@ -0,0 +1,16 @@
+module dff(
+input clk,
+input rst,
+input din,  
+output reg dout  
+);
+  
+  always@(posedge clk)
+    begin
+      if(rst)
+         dout <= 1'b0;
+      else
+         dout <= din;
+    end
+  
+endmodule
